68c92df96c766089d119c0ae5cab6049855d0320
pborisp <popovbs@gmail.com>
2025-06-29 23:34:52 +0300

    Kurs Java COR

---
+++ src/main/java/org/skypro/quesrionsForExam/QuesrionsForExamApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.quesrionsForExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuesrionsForExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuesrionsForExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/skypro/quesrionsForExam/QuesrionsForExamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.quesrionsForExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuesrionsForExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
